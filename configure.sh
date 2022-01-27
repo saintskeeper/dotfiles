b6898511687f23dfca6eeade24211f4fa515d238
saintskeeper <walterdayjr@gmail.com>
2022-01-27 18:58:12 -0500

    fixes home brew  config for mac

--- configure.sh
+++ configure.sh
@@ -3,3 +3,13 @@
 for i in `cat files-to-link.txt`;
 do ln -s $i ~/$i;
 done
+
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
+
+# need a script to return true of uname is Darwin
+
+unamestr=$(uname)
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+	echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
+fib
